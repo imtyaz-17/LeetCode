014251ce405d0a025bffa153ab601a3b560653d8
Imtyaz Ahmed <54882826+imtyaz-17@users.noreply.github.com>
2022-11-29 00:49:18 +0600

    Time: 15 ms (57.97%), Space: 13.3 MB (31.65%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -4,32 +4,27 @@ class Solution
         int countSubstrings(string s)
         {
             int n = s.size();
-            if(n==0) return 0;
-            bool dp[n][n];
+            if (n == 0) return 0;
             int count = 0;
-            
-            for(int len =1;len<=n;len++){
-                for(int i=0,j=i+len-1;j<n;i++,j++){
-                    if(len==1){
-                        dp[i][i] =true;
-                        count++;
-                    }
-                    else if(len==2){
-                        dp[i][i+1]=s[i]==s[i+1];
-                        if(dp[i][i+1]){
-                            count++;
-                        }
-                    }
-                    else{
-                        dp[i][j]=(s[i]==s[j] && dp[i+1][j-1]);
-                        if(dp[i][j]){
-                            count++;
-                        }
-                    }
-                }
+            char c[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                count += isPalindrome(i, i, s);
+                count += isPalindrome(i, i + 1, s);
             }
-            
 
             return count;
         }
+
+    int isPalindrome(int st, int ed, string c)
+    {
+        int ct = 0;
+        while (st >= 0 && ed < c.size() && c[st--] == c[ed++])
+        {
+            ct++;
+        }
+
+        return ct;
+    }
 };
