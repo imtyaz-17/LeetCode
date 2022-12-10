a77edfab8a42d0537371a3be7007bc24a6f8ba46
Imtyaz Ahmed <54882826+imtyaz-17@users.noreply.github.com>
2022-12-10 17:46:37 +0600

    Time: 12 ms (96.89%), Space: 13.4 MB (20.50%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+    public:
+        void setZeroes(vector<vector < int>> &matrix)
+        {
+            int r = matrix.size();
+            int c = matrix[0].size();
+
+            vector<int> x, y;
+
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    if (matrix[i][j] == 0)
+                    {
+                        x.push_back(i);
+                        y.push_back(j);
+                    }
+                }
+            }
+
+           	// row->0
+            for (int i = 0; i < x.size(); i++)
+            {
+                int index = x[i];
+                for (int j = 0; j < c; j++)
+                {
+                    matrix[index][j] = 0;
+                }
+            }
+
+           	// col->0
+            for (int i = 0; i < y.size(); i++)
+            {
+                int index = y[i];
+                for (int j = 0; j < r; j++)
+                {
+                    matrix[j][index] = 0;
+                }
+            }
+        }
+};
