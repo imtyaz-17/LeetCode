814ea18d757c161b72ebf0d44dab65042bd18eb5
Imtyaz Ahmed <54882826+imtyaz-17@users.noreply.github.com>
2022-12-22 10:07:59 +0600

    Added solution - LeetHub

---
+++ Professor and Parties - GFG/professor-and-parties.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    
+    string PartyType(long long int a[], int n)
+    {
+        // Your code goes here
+        
+        
+        unordered_set<int> mySet;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (mySet.find(a[i]) != mySet.end())
+        {
+            return "BOYS";
+        }
+        mySet.insert(a[i]);
+    }
+    return "GIRLS";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+
+        int n;
+        cin >> n;
+        long long int a[n+5];
+        for(int i =0;i<n;i++)
+            cin >> a[i];
+        Solution ob;
+        cout<< ob.PartyType(a, n) <<endl;
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
