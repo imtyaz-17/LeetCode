f776b6831dc9082d20a62962a64d758968071d3c
Imtyaz Ahmed <54882826+imtyaz-17@users.noreply.github.com>
2023-01-25 15:10:29 +0600

    Time: 0 ms, Memory: 6 MB - LeetHub

---
+++ reverse-integer/reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+      
+    long reverse_int = 0;
+    while (x !=0) {
+        int last_digit = x % 10;
+        reverse_int = reverse_int * 10 + last_digit;
+        x = x / 10;
+    }
+    if (reverse_int > 0x7FFFFFFF || reverse_int < -0x7FFFFFFF){
+        return 0;
+    }
+    return (int) reverse_int;
+    }
+};
