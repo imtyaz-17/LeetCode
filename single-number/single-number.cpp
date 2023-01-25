81296c43fcdda140c44a325e4ee0d74b72a0c729
Imtyaz Ahmed <54882826+imtyaz-17@users.noreply.github.com>
2023-01-25 12:11:58 +0600

    Time: 42 ms, Memory: 16.9 MB - LeetHub

--- single-number/single-number.cpp
+++ single-number/single-number.cpp
@@ -2,17 +2,24 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
-        unordered_map<int ,int>mp;
+//         unordered_map<int ,int>mp;
         
-        for(int i:nums)
-            mp[i]++;
+//         for(int i:nums)
+//             mp[i]++;
         
-        for(auto it:mp){
-            if(it.second==1){
-                return it.first;
-            }
+//         for(auto it:mp){
+//             if(it.second==1){
+//                 return it.first;
+//             }
+//         }
+        
+//         return -1;
+        
+        
+        int ans = 0;
+        for(int x: nums) {
+            ans ^= x;
         }
-        
-        return -1;
+        return ans;
     }
 };
