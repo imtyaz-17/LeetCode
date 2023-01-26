6d2ae4fa807bcb863d3992de81ced2f0f313d722
Imtyaz Ahmed <54882826+imtyaz-17@users.noreply.github.com>
2023-01-27 00:38:42 +0600

    Time: 0 ms, Memory: 8 MB - LeetHub

--- fizz-buzz/fizz-buzz.cpp
+++ fizz-buzz/fizz-buzz.cpp
@@ -3,14 +3,15 @@ public:
     vector<string> fizzBuzz(int n) {
         vector<string>ans;
         for(int i=1;i<=n;i++){
-            if(i%3==0 && i%5==0){
+            if(i%15==0){  // 3 & 5
                 ans.push_back("FizzBuzz");
+                
+            }
+            else if(i%3==0){
+                 ans.push_back("Fizz");
             }
             else if(i%5==0){
                 ans.push_back("Buzz");
-            }
-            else if(i%3==0){
-                ans.push_back("Fizz");
             }
             else{
                 ans.push_back(to_string(i));
