7da2940b4bf3d90e817bd9add9e832a587d771dc
Imtyaz Ahmed <54882826+imtyaz-17@users.noreply.github.com>
2023-01-27 16:01:59 +0600

    Time: 3 ms (55.22%), Space: 5.9 MB (56.03%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -2,11 +2,6 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         
-        if(n==1) 
-            return true;
-        if(n==0 || n%4!=0)
-            return false;
-        
-        return isPowerOfFour(n/4);
+        return n> 0 && (n & (n-1))==0 && (n-1)%3 ==0;
     }
 };
