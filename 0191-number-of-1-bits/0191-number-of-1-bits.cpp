753065cce71b2e316b3967c33670406762785256
Imtyaz Ahmed <54882826+imtyaz-17@users.noreply.github.com>
2023-01-27 15:16:31 +0600

    Time: 0 ms (100.00%), Space: 5.8 MB (91.28%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
+        // int ones=0;
+        // while (n){
+        //     if (n&1){
+        //         ones++;
+        //     }
+        //     n = n>>1; // n/=2
+        // }
+        // return ones;
+        
+        
+        //#2 Flip least 1 bit
         int ones=0;
         while (n){
-            if (n&1){
                 ones++;
-            }
-            n = n>>1; // n/=2
+                n = n &(n-1);
         }
         return ones;
+        
     }
 };
