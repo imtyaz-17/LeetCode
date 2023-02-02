d2dc2c021238d9a4f4669c589695e8635a464c9e
Imtyaz Ahmed <54882826+imtyaz-17@users.noreply.github.com>
2023-02-02 20:00:32 +0600

    Time: 24 ms, Memory: 19.2 MB - LeetHub

--- move-zeroes/move-zeroes.cpp
+++ move-zeroes/move-zeroes.cpp
@@ -3,20 +3,35 @@ public:
     void moveZeroes(vector<int>& nums) {
         int len = nums.size();
         
-        int zeroCount=0;
-        int j=0;
-        for(int i=0;i<len;i++){
-            if(nums[i]!=0){
-                nums[j]=nums[i];
-                j++;
-            }
-            else{
-                zeroCount++;
-            }
-        }
+//         int zeroCount=0;
+//         int j=0;
+//         for(int i=0;i<len;i++){
+//             if(nums[i]!=0){
+//                 nums[j]=nums[i];
+//                 j++;
+//             }
+//             else{
+//                 zeroCount++;
+//             }
+//         }
         
-        for(int i=0;i<zeroCount;i++){
-            nums[j++]=0;
+//         for(int i=0;i<zeroCount;i++){
+//             nums[j++]=0;
+//         }
+        
+        if(len==0 || len==1)
+            return;
+        
+        int left=0,right=0;
+        
+        while(right<len){
+            if(nums[right]==0){
+                right++;
+            }else{
+                swap(nums[left],nums[right]);
+                left++;
+                right++;
+            }
         }
     }
 };
