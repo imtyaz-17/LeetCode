c705f902c6b456ff3c159d0cf8995b8032530d5f
Imtyaz Ahmed <54882826+imtyaz-17@users.noreply.github.com>
2023-02-10 10:31:00 +0600

    Time: 48 ms (44.25%), Space: 20.8 MB (34.87%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> map;
+        unordered_map<string, vector<string>> u_map;
         vector<vector<string>> ans;
-        int i=0;
+        
         for(string s : strs){
+            string temp = s;
             sort(s.begin(), s.end());
-            map[s].push_back(strs[i++]);
+            u_map[s].push_back(temp);
         }
         
-        for(auto p : map){
-            sort(p.second.begin(), p.second.end());
-            ans.push_back(p.second);
+        for(auto x : u_map){
+           // vector<string>temp =x.second;
+            ans.push_back(x.second);
         }
         
         return ans;
     }
 };
+
+//**
+// https://youtu.be/zX9k1XvVUXI
