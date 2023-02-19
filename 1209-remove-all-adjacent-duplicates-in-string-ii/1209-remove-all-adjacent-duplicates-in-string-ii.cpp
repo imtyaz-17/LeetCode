dd9ef00e7e1ef079e92a0896933b112cf25e7844
Imtyaz Ahmed <54882826+imtyaz-17@users.noreply.github.com>
2023-02-19 20:42:50 +0600

    Time: 16 ms (86.46%), Space: 10.7 MB (45.82%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+    public:
+        string removeDuplicates(string s, int k)
+        {
+            int n = s.size();
+
+            vector<pair<char, int>> vpair;
+
+            for (auto c: s)
+            {
+                if (vpair.empty() || vpair.back().first != c)
+                {
+                    vpair.push_back({ c,
+                        1 });
+                }
+                else
+                {
+                    vpair.back().second++;
+
+                    if (vpair.back().second == k)
+                    {
+                        vpair.pop_back();
+                    }
+                }
+            }
+
+            string result;
+
+            for (auto x: vpair)
+            {
+                result.append(x.second, x.first);
+            }
+
+            return result;
+        }
+};
