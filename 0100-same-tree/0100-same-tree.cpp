515b60979a4bac45054b5b997266587b91ada939
Imtyaz Ahmed <54882826+imtyaz-17@users.noreply.github.com>
2023-03-04 14:59:28 +0600

    Time: 2 ms (58.54%), Space: 10.1 MB (8.67%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -1,38 +1,22 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *  int val;
- *  TreeNode * left;
- *  TreeNode * right;
- *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *int val;
+ *TreeNode * left;
+ *TreeNode * right;
+ *TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-        void preOrder(TreeNode *root, vector<int> &preOrderNodes)
+        bool isSameTree(TreeNode *p, TreeNode *q)
         {
-            if (root != NULL)
-            {
-                preOrderNodes.push_back(root->val);
-                preOrder(root->left, preOrderNodes);
-                preOrder(root->right, preOrderNodes);
-            }
-            else
-            {
-                preOrderNodes.push_back(100000);
-            }
-        }
-    bool isSameTree(TreeNode *p, TreeNode *q)
-    {
-        vector<int> preP, preQ;
-        preOrder(p, preP);
-        preOrder(q, preQ);
+            if (p == NULL && q == NULL) return true;
+            if (p == NULL || q == NULL) return false;
 
-        if (preP == preQ) return true;
-        else
-            return false;
-    }
+            return (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+        }
 };
